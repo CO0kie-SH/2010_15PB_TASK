92cafbf7be958c8aca7169c1102ea67a268c4142
mhws2 <mhws2@MHWS2>
2020-10-07 11:07:25 +0800

    添加项目文件。

---
+++ 2010_15PB_TASK/2010_15PB_TASK.h
@@ -0,0 +1,32 @@
+﻿
+// 2010_15PB_TASK.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMy201015PBTASKApp:
+// 有关此类的实现，请参阅 2010_15PB_TASK.cpp
+//
+
+class CMy201015PBTASKApp : public CWinApp
+{
+public:
+	CMy201015PBTASKApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMy201015PBTASKApp theApp;
---
+++ 2010_15PB_TASK/2010_15PB_TASKDlg.h
@@ -0,0 +1,34 @@
+﻿
+// 2010_15PB_TASKDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CMy201015PBTASKDlg 对话框
+class CMy201015PBTASKDlg : public CDialogEx
+{
+// 构造
+public:
+	CMy201015PBTASKDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MY2010_15PB_TASK_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
